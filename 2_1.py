7dd83362c3171edf29fc4a8b02301429fc0723ce
abhinav0343 <abhinavgolla2007@gmail.com>
2025-01-23 15:09:33 +0530

    Delete 2_1.py

--- 2_1.py
+++
@@ -1,9 +0,0 @@
-name=[]
-x = list(input("enter the name "))
-for i in range (len(x)) :
-    if i%2 != 0:
-        name.append(x[i].capitalize()) 
-    else :
-        name.append(x[i])      
-result = ''.join(name)   
-print(result)
