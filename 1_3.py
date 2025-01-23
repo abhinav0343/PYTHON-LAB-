b65a100874b175010d6b45eaa60aab5a57f4ff3f
abhinav0343 <abhinavgolla2007@gmail.com>
2025-01-23 15:08:26 +0530

    Delete 1_3.py

--- 1_3.py
+++
@@ -1,9 +0,0 @@
-num = float(input("enter the length in feet "))
-units = [ 'inches' , 'yards' , 'miles' , 'millimeters' , 'centimeters' , 'meters' , 'kilometers']
-conversion = [ 12,0.333,0.0001893939,304.8,30.48,0.3048,0.0003048]
-print("enter the number according to mentioned above ")
-for i in range (0,7) :
-    print(f"{i+1} for {units[i]}")
-value = int(input("enter your choice "))
-con = num * conversion[value - 1] 
-print(f" {num} feets is equal to {con} {units[value - 1]}")
