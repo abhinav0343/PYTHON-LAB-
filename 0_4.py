958004bba92b319544833046ec374f3b96eafa5c
abhinav0343 <abhinavgolla2007@gmail.com>
2025-01-23 15:07:16 +0530

    Delete 0_4.py

--- 0_4.py
+++
@@ -1,6 +0,0 @@
-print("to swap two variables without using third variable\n")
-num1 = float(input("enter the first number : "))
-num2 = float(input("enter the second number : "))
-print("before swaping num1 =",num1,"num2 =",num2)
-num1,num2 = num2,num1
-print("after swaping num1 =",num1,"num2 =",num2)
