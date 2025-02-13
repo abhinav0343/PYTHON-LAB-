339a94aaee88824819f0cfdef23f2ef0f81ee4fa
ABHINAV KUMAR <abhinavgolla2007@gmail.com>
2025-02-13 14:53:39 +0530

    Delete Assignment5 directory

--- Assignment5/5_1.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("enter the first value : "))
-num2 = int(input("enter the second value : ")) + 1
-xor_max = []
-for i in range (num1,num2) :
-    for j in range (i,num2) :
-        val = i ^ j
-        xor_max.append(val)
-print(f"{max(xor_max)}")
